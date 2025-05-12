2f2898e46ad475f4a25bc10a2f29f4fbde93c3b3
zeroheight <tokens-management@zeroheight.com>
2025-05-12 10:09:53 +0100

    Update laura figma zh test from zeroheight

---
+++ test-delete-12may/token_Tokens_Dark.dart
@@ -0,0 +1,26 @@
+
+//
+// 
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const colorBorderBorderFocused = Color(0xFFFDF1DF);
+    static const colorBorderBorderPrimary = Color(0xFFFFF5F1);
+    static const colorBorderBorderSecondary = Color(0x40FFF5F1);
+    static const colorButtonBorderSecondary = Color(0xFFFDF1DF);
+    static const colorButtonSurfacePrimary = Color(0xFFFDF1DF);
+    static const colorButtonSurfaceSecondary = Color(0xFF6B2106);
+    static const colorButtonTextOnprimary = Color(0xFF481604);
+    static const colorColor = Color(0xFFFFFFFF);
+    static const colorSurfaceSurfaceBrand = Color(0xFFC03B0B);
+    static const colorSurfaceSurfacePrimary = Color(0xFF481604);
+    static const colorSurfaceSurfaceSecondary = Color(0xFF6B2106);
+    static const colorTextTextAction = Color(0xFFFDF1DF);
+    static const colorTextTextPrimary = Color(0xFFFFFDFC);
+    static const colorTextTextSecondary = Color(0xFFFFF5F1);
+}
