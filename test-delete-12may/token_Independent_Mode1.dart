5f58a001aaae4f562622c00f72adc13ed9450663
zeroheight <tokens-management@zeroheight.com>
2025-05-12 10:09:53 +0100

    Update laura figma zh test from zeroheight

---
+++ test-delete-12may/token_Independent_Mode1.dart
@@ -0,0 +1,17 @@
+
+//
+// 
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const colorBlue50 = Color(0x66228EF0); /* it's "blue" 😀 */
+    static const colorNewGroupRreed = Color(0xFFE33A3A);
+    static const colorNewGroupWhhyyyttee = Color(0xFFFFFFF0);
+    static const colorTransparecntGrey = Color(0x3D000000);
+    static const contentString = "Stringy";
+}
