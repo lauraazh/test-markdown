849ffa1f5192e4066a904fe5a52199dbbd065289
zeroheight <tokens-management@zeroheight.com>
2025-05-12 10:09:54 +0100

    Update laura figma zh test from zeroheight

---
+++ test-delete-12may/token_IndependentLinked_Mode1.dart
@@ -0,0 +1,15 @@
+
+//
+// 
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const colorLinkedRead = Color(0xFFE33A3A);
+    static const colorSurfFaceSceond = Color(0xFF6B2106);
+    static const colorWite = Color(0xFFFFFFFF);
+}
